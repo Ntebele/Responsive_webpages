d821f7bb04eba81ec7f650adb1e38f6e62ef16b3
Ntebele <mahamochristopher25@gmail.com>
2023-05-31 16:08:56 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
-import handlebars from "vite-plugin-handlebars";
+import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
 import data from './data.json';
-const basicContext = {
-        title : "Hello this is some text",
-        show : false,
-        fruits : [
-            "apples",
-            "oranges",
-            "bananas"
-        ],
-        isClient : true,
-        fruitSales : [
-            {day : "Monday", apples : 13, oranges : 34},
-            {day : "Tuesday", apples : 17, oranges : 28},
-            {day : "Thursday", apples : 7, oranges : 45},
-        ]
-    };
 
-    export default {
-  plugins: [handlebars({
-    context :data
+export default defineConfig({
+    base : '/responsive_webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+                bootstrap: 'index-bootstrap.html',
+                materialize: 'index-materialize.html',
+            }
+        },
+    },
+    plugins: [handlebars({
+      context : {
+        data
+      }
   })]
-};
+    
+});
